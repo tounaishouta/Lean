d3407604164875404c05badb1e14f7c83d166089
TOUNAI Shouta <tounai.shouta@gmail.com>
2016-02-01 20:49:22 +0900

    initial commit

---
+++ 2-8_3-1.lean
@@ -0,0 +1,213 @@
+import standard
+
+print "\n Pi type in Lean \n"
+
+namespace hide1
+
+constant A : Type
+constant B : A → Type
+
+check Π (x : A), B x -- backslash P
+check Π x : A, B x -- same as above
+check Pi x : A, B x -- same as above
+
+constant C : A → A → Type
+
+check Π x : A, Π y : A, C x y
+check Π (x : A) (y : A), C x y -- same as above
+check Π (x y : A), C x y -- same as above
+check Π x y : A, C x y -- same as above
+
+constant D : Type
+
+check A → D
+check Π x : A, D -- same as above
+
+end hide1
+
+print "\n Let's define `identity` \n"
+
+constant n : ℕ
+constant b : bool
+
+print "\n 1. それぞれ作る \n"
+
+definition id_nat (n : ℕ) : ℕ := n
+check id_nat
+eval id_nat n
+
+definition id_bool (b : bool) : bool := b
+check id_bool
+eval id_bool b
+
+-- 全部するのは面倒だし無駄 DRY
+
+print "\n 2. Use Pi type \n"
+
+definition id1 (X : Type) (x : X) : X := x
+/-
+definition id1
+: Π X : Type, X → X
+:= λ (X : Type) (x : X), x
+-/
+check id1
+
+eval id1 ℕ n
+
+eval id1 bool b
+
+print "\n 3. Use underscore _ \n"
+
+check id1 _ n
+eval id1 _ n
+eval id1 _ b
+
+print "\n 4. Use curly brace { } \n"
+
+definition id2 {X : Type} (x : X) : X := x
+/-
+definition id2
+: Π {X : Type}, X → X
+:= λ (X : Type) (x : X), x
+-/
+
+check id2
+eval id2 n
+eval id2 b
+
+print "\n use @ \n"
+
+check @id2 -- @ を使うと implicit が explicit に
+eval @id2 ℕ n
+eval @id2 bool b
+
+print "\n use ! \n"
+
+check !id1 -- ! はその逆
+eval !id1 n
+eval !id1 b
+
+print "\n Let's define `composition` \n"
+
+definition comp {A B C : Type} (g : B → C) (f : A → B) (x : A) : C := g (f x)
+check comp
+check @comp
+eval comp (λ x : ℕ, x + 5) (λ x : ℕ, x + 7) 8
+
+definition do_twice {A : Type} (f : A → A) := comp f f
+check do_twice
+check @do_twice
+eval do_twice (λ x : ℕ, x + 3) 5
+
+print "\n library の list についてみてみよう \n"
+
+open list
+
+check list
+check nil
+check @nil
+check cons
+check @cons
+check head
+check @head
+check tail
+check @tail
+check append
+check @append
+
+print "\n 上を参考に vec について考えてみよう \n"
+-- vec A n は A の元を n 個並べたものの型
+-- 数学における A^n
+namespace hide2
+
+constant vec : Type → ℕ → Type
+
+namespace vec
+constant nil : Π {X : Type}, vec X 0
+constant cons : Π {X : Type} {n : ℕ}, X → vec X n → vec X (n + 1)    
+constant append : Π {X : Type} {n m : ℕ}, vec X n → vec X m → vec X (n + m)
+
+open bool
+
+constant v1 : vec bool 10
+constant v2 : vec bool 20
+check cons tt nil
+check cons ff v1
+check append v1 v2
+
+end vec
+end hide2
+
+print "\n Sigma type in Lean \n"
+
+namespace hide3
+open sigma
+
+constant A : Type
+constant B : A → Type
+constant C : Type
+constant a : A
+constant b : B a
+
+check sigma
+check @sigma
+
+check sigma B
+check Σ x : A, B x -- same as above (backslash S)
+
+check sigma.mk
+check @sigma.mk
+
+check sigma.mk a b
+check ⟨a, b⟩ -- smae as above  (backslash less, backslash greater)
+
+check A × C
+check Σ x : A, C -- same as above <- wrong
+
+check pr1 -- pr₁ でも OK (pr backslash undersocre 1)
+check @pr1
+check pr2 -- pr₂ でも OK
+check @pr2
+
+check pr1 ⟨a, b⟩
+check pr2 ⟨a, b⟩
+eval pr1 ⟨a, b⟩
+eval pr2 ⟨a, b⟩
+
+end hide3
+
+print "\n Prop in Lean \n"
+
+check Prop
+check Type.{0}
+
+theorem HilbertS {P Q R : Prop}
+: (P → Q → R) → (P → Q) → P → R
+:=
+assume g,
+assume f,
+assume x,
+show R, from g x (f x)
+
+check HilbertS
+
+check fun (A : Type) (a b : A), a = b
+
+
+constant A : Prop
+
+theorem p1 : A → A → A :=
+assume Ha1 : A,
+assume Ha2 : A,
+show A, from Ha1
+
+theorem p2 : A → A → A :=
+assume Ha1 : A,
+assume Ha2 : A,
+show A, from Ha2
+
+check p1
+check p2
+
+example : p1 = p2 := rfl
+-- p1 と p2 は別の証明だが等号を示せる。
---
+++ 3-6.lean
@@ -0,0 +1,348 @@
+open classical
+
+variables p q r s : Prop
+
+-- commutativity of ∧ and ∨
+example : p ∧ q ↔ q ∧ p :=
+have and_comm : ∀ r s : Prop, r ∧ s → s ∧ r, from
+  assume r s : Prop,
+  assume Hrs : r ∧ s,
+  have Hr : r, from and.left Hrs,
+  have Hs : s, from and.right Hrs,
+  show s ∧ r, from and.intro Hs Hr,
+iff.intro (and_comm p q) (and_comm q p)
+
+example : p ∨ q ↔ q ∨ p :=
+have or_comm : ∀ r s : Prop, r ∨ s → s ∨ r, from
+  assume r s : Prop,
+  assume Hrs : r ∨ s,
+  show s ∨ r, from
+    or.elim Hrs
+      (assume Hr : r, or.inr Hr)
+      (assume Hs : s, or.inl Hs),
+iff.intro (or_comm p q) (or_comm q p)
+
+-- associativity of ∧ and ∨
+example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) :=
+iff.intro (
+  assume H : (p ∧ q) ∧ r,
+  have Hp : p, from and.left (and.left H),
+  have Hq : q, from and.right (and.left H),
+  have Hr : r, from and.right H,
+  show p ∧ (q ∧ r), from and.intro Hp (and.intro Hq Hr)
+) (
+  assume H : p ∧ (q ∧ r),
+  have Hp : p, from and.left H,
+  have Hq : q, from and.left (and.right H),
+  have Hr : r, from and.right (and.right H),
+  show (p ∧ q) ∧ r, from and.intro (and.intro Hp Hq) Hr
+)
+
+-- distributivity
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) :=
+iff.intro (
+  assume H : p ∧ (q ∨ r),
+  have Hp : p, from and.left H,
+  have Hqr : q ∨ r, from and.right H,
+  show (p ∧ q) ∨ (p ∧ r), from or.elim Hqr
+    (assume Hq : q, or.inl (and.intro Hp Hq))
+    (assume Hr : r, or.inr (and.intro Hp Hr))
+) (
+  assume H : (p ∧ q) ∨ (p ∧ r),
+  show p ∧ (q ∨ r), from or.elim H (
+    assume Hpq : p ∧ q,
+    have Hp : p, from and.left Hpq,
+    have Hq : q, from and.right Hpq,
+    and.intro Hp (or.inl Hq)
+  ) (
+    assume Hpr : p ∧ r,
+    have Hp : p, from and.left Hpr,
+    have Hr : r, from and.right Hpr,
+    and.intro Hp (or.inr Hr)
+  )
+)
+
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) :=
+iff.intro (
+  assume H : p ∨ (q ∧ r),
+  show (p ∨ q) ∧ (p ∨ r), from or.elim H (
+    assume Hp : p,
+    and.intro (or.inl Hp) (or.inl Hp)
+  ) (
+    assume Hqr : q ∧ r,
+    have Hq : q, from and.left Hqr,
+    have Hr : r, from and.right Hqr,
+    and.intro (or.inr Hq) (or.inr Hr)
+  )
+) (
+  assume H : (p ∨ q) ∧ (p ∨ r),
+  have Hpq : p ∨ q, from and.left H,
+  have Hpr : p ∨ r, from and.right H,
+  show p ∨ (q ∧ r), from
+    or.elim Hpq (
+      assume Hp : p,
+      or.inl Hp
+    ) (
+      assume Hq : q,
+      or.elim Hpr (
+        assume Hp : p,
+        or.inl Hp
+      ) (
+        assume Hr : r,
+        or.inr (and.intro Hq Hr)
+      )
+    )
+)
+
+-- other properties
+example : (p → (q → r)) ↔ (p ∧ q → r) :=
+iff.intro (
+  assume H : p → q → r,
+  assume Hpq : p ∧ q,
+  have Hp : p, from and.left Hpq,
+  have Hq : q, from and.right Hpq,
+  show r, from H Hp Hq
+) (
+  assume H : p ∧ q → r,
+  assume Hp : p,
+  assume Hq : q,
+  show r, from H (and.intro Hp Hq)
+)
+
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) :=
+iff.intro (
+  assume H : p ∨ q → r,
+  show (p → r) ∧ (q → r), from and.intro (
+    assume Hp : p,
+    H (or.inl Hp)
+  ) (
+    assume Hq : q,
+    H (or.inr Hq)
+  )
+) (
+  assume H : (p → r) ∧ (q → r),
+  have Hpr : p → r, from and.left H,
+  have Hqr : q → r, from and.right H,
+  assume Hpq : p ∨ q,
+  show r, from or.elim Hpq (
+    assume Hp : p, Hpr Hp
+  ) (
+    assume Hq : q, Hqr Hq
+  )
+)
+
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q :=
+iff.intro (
+  assume H : ¬(p ∨ q),
+  show ¬p ∧ ¬q, from and.intro (
+    not.intro (
+      assume Hp : p,
+      absurd (or.inl Hp) H
+    )
+  ) (
+    not.intro (
+      assume Hq : q,
+      absurd (or.inr Hq) H
+    )
+  )
+) (
+  assume H : ¬p ∧ ¬q,
+  have Hnp : ¬p, from and.left H,
+  have Hnq : ¬q, from and.right H,
+  show ¬(p ∨ q), from not.intro (
+    assume Hpq : p ∨ q,
+    or.elim Hpq (
+      assume Hp : p,
+      absurd Hp Hnp
+    ) (
+      assume Hq : q,
+      absurd Hq Hnq
+    )
+  )
+)
+
+example : ¬p ∨ ¬q → ¬(p ∧ q) :=
+assume H : ¬p ∨ ¬q,
+show ¬(p ∧ q), from not.intro ( 
+  assume Hpq : p ∧ q,
+  have Hp : p, from and.left Hpq,
+  have Hq : q, from and.right Hpq,
+  or.elim H (
+    assume Hnp : ¬p,
+    absurd Hp Hnp
+  ) (
+    assume Hnq : ¬q,
+    absurd Hq Hnq
+  )
+)
+
+example : ¬(p ∧ ¬ p) :=
+not.intro (
+  assume H : p ∧ ¬p,
+  have Hp : p, from and.left H,
+  have Hnp : ¬p, from and.right H,
+  absurd Hp Hnp
+)
+
+example : p ∧ ¬q → ¬(p → q) :=
+assume H : p ∧ ¬q,
+have Hp : p, from and.left H,
+have Hnq : ¬q, from and.right H,
+show ¬(p → q), from not.intro (
+  assume Hpq : p → q,
+  absurd (Hpq Hp) Hnq
+)
+  
+example : ¬p → (p → q) :=
+assume Hnp : ¬p,
+assume Hp : p,
+show q, from absurd Hp Hnp
+
+example : (¬p ∨ q) → (p → q) :=
+assume H : ¬p ∨ q,
+assume Hp : p,
+show q, from or.elim H (
+  assume Hnp : ¬p,
+  absurd Hp Hnp
+) (
+  assume Hq,
+  Hq
+)
+
+example : p ∨ false ↔ p :=
+iff.intro (
+  assume H : p ∨ false,
+  show p, from or.elim H (
+    assume Hp : p,
+    Hp
+  ) (
+    assume Hf : false,
+    false.elim Hf
+  )
+) (
+  assume Hp : p,
+  show p ∨ false, from or.inl Hp
+)
+
+example : p ∧ false ↔ false :=
+iff.intro (
+  assume H : p ∧ false,
+  show false, from and.right H
+) (
+  assume Hf : false,
+  show p ∧ false, from false.elim Hf
+)
+
+example : ¬(p ↔ ¬p) :=
+not.intro (
+  assume H : p ↔ ¬p,
+  have Hnp : ¬p, from not.intro (       
+    assume Hp : p,
+    absurd Hp (iff.mp H Hp)
+  ),
+  show false, from absurd (iff.mpr H Hnp) Hnp
+)
+
+example : (p → q) → (¬q → ¬p) :=
+assume H : p → q,
+assume Hnq : ¬q,
+show ¬p, from not.intro (
+  assume Hp : p,
+  absurd (H Hp) Hnq
+)
+
+-- these require classical reasoning
+example : (p → r ∨ s) → ((p → r) ∨ (p → s)) :=
+assume H : p → r ∨ s,
+show (p → r) ∨ (p → s), from by_cases (
+  assume Hr : r,
+  or.inl (
+    assume Hp : p,
+    Hr
+  )
+) (
+  assume Hnr : ¬r,
+  or.inr (
+    assume Hp : p,
+    show s, from or.elim (H Hp) (
+      assume Hr : r,
+      absurd Hr Hnr
+    ) (
+      assume Hs : s,
+      Hs
+    )   
+  )
+)
+
+example : ¬(p ∧ q) → ¬p ∨ ¬q :=
+assume H : ¬(p ∧ q),
+show ¬p ∨ ¬q, from by_cases (
+  assume Hp : p,
+  have Hnq : ¬q, from not.intro (
+    assume Hq : q,
+    absurd (and.intro Hp Hq) H
+  ),
+  or.inr Hnq
+) (
+  assume Hnp : ¬p,
+  or.inl Hnp
+)
+    
+example : ¬(p → q) → p ∧ ¬q :=
+assume H : ¬(p → q),
+and.intro (
+  show p, from by_cases (
+    assume Hp : p,
+    Hp
+  ) (
+    assume Hnp : ¬p,
+    have Hpq : p → q, from (
+      assume Hp : p,
+      absurd Hp Hnp
+    ),
+    absurd Hpq H
+  )
+) (
+  show ¬q, from not.intro (
+    assume Hq : q,
+    have Hpq : p → q, from (
+      assume Hp : p,
+      Hq
+    ),
+    absurd Hpq H
+  )
+)
+
+example : (p → q) → (¬p ∨ q) :=
+assume Hpq : p → q,
+show ¬p ∨ q, from by_cases (
+  assume Hp : p,
+  or.inr (Hpq Hp)
+) (
+  assume Hnp : ¬p,
+  or.inl Hnp
+)
+
+example : (¬q → ¬p) → (p → q) :=
+assume H : ¬q → ¬p,
+assume Hp : p,
+show q, from by_contradiction (
+  assume Hnq : ¬q,
+  absurd Hp (H Hnq)
+)
+
+example : p ∨ ¬p := em p
+
+example : (((p → q) → p) → p) :=
+assume H : (p → q) → p,
+show p, from by_cases (
+  assume Hp : p,
+  Hp
+) (
+  assume Hnp : ¬p,
+  have Hpq : p → q, from (
+    assume Hp : p,
+    absurd Hp Hnp
+  ),
+  H Hpq
+)
