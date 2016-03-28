7b5fab186dc29bb47409aef741760ca4d78cd4ec
TOUNAI Shouta <tounai.shouta@gmail.com>
2016-03-29 04:02:47 +0900

    modify

--- category.lean
+++ category.lean
@@ -1,116 +1,137 @@
 import standard
 
+namespace category
+
 structure Category :=
   (Ob : Type)
   (Hom : Ob → Ob → Type)
   (comp : Π {a b c : Ob}, Hom b c → Hom a b → Hom a c)
-  (id : Π {a : Ob}, Hom a a)
+  (id : Π a : Ob, Hom a a)
   (comp_assoc : ∀ {a b c d : Ob} {h : Hom c d} {g : Hom b c} {f : Hom a b},
                 comp (comp h g) f = comp h (comp g f))
-  (comp_id : ∀ {a b : Ob} {f : Hom a b}, comp f id = f)
-  (id_comp : ∀ {a b : Ob} {f : Hom a b}, comp id f = f)
+  (comp_id : ∀ {a b : Ob} {f : Hom a b}, comp f (id a) = f)
+  (id_comp : ∀ {a b : Ob} {f : Hom a b}, comp (id b) f = f)
 
-namespace Category
+attribute Category.Ob [coercion]
+notation a `⇒` b := Category.Hom _ a b
+notation g `∘` f := Category.comp _ g f
+notation 1 := Category.id _ _
 
-attribute Ob [coercion]
+example (C : Category) (a b c d : C) (h : c ⇒ d) (g : b ⇒ c) (f : a ⇒ b) :
+  (h ∘ g) ∘ f = h ∘ (g ∘ f) :=
+  Category.comp_assoc C
 
-notation g `∘` f := comp _ g f
+example (C : Category) (a b : C) (f : a ⇒ b) : f ∘ 1 = f := Category.comp_id C
 
-example (C : Category) (a b c d : C) (h : Hom C c d) (g : Hom C b c) (f : Hom C a b)
-  : (h ∘ g) ∘ f = h ∘ (g ∘ f)
-  := Category.comp_assoc C
+example (C : Category) (a b : C) (f : a ⇒ b) : 1 ∘ f = f := Category.id_comp C
 
-example (C : Category) (a b : C) (f : Hom C a b)
-  : f ∘ id C = f
-  := Category.comp_id C
+definition op (C : Category) : Category :=
+  ⦃ Category,
+    Ob := Category.Ob C,
+    Hom := λ a b, Category.Hom C b a,
+    comp := λ a b c g f, Category.comp C f g,
+    id := λ a, Category.id C a,
+    comp_assoc := take a b c d h g f, eq.symm (Category.comp_assoc C),
+    comp_id := take a b f, Category.id_comp C,
+    id_comp := take a b f, Category.comp_id C ⦄
 
-example (C : Category) (a b : C) (f : Hom C a b)
-  : id C ∘ f = f
-  := Category.id_comp C
+definition Type_as_Cat : Category :=
+  ⦃ Category,
+    Ob := Type,
+    Hom := λ a b, a → b,
+    comp := λ a b c g f, λ x, g (f x),
+    id := λ a, λ x, x,
+    comp_assoc := take a b c d h g f, rfl,
+    comp_id := take a b f, rfl,
+    id_comp := take a b f, rfl ⦄
 
-definition op [reducible] (C : Category)
-  : Category
-  := ⦃ Category
-     , Ob := Ob C
-     , Hom := λ a b : C, Hom C b a
-     , comp := λ a b c g f, f ∘ g
-     , id := λ a, id C
-     , comp_assoc
-       := take a b c d h g f, eq.symm (comp_assoc C)
-     , comp_id
-       := take a b f, id_comp C
-     , id_comp
-       := take a b f, comp_id C
-     ⦄
-  
-definition Type_as_Category : Category :=
-  ⦃ Category
-  , Ob := Type
-  , Hom := λ X Y : Type, X → Y
-  , comp := λ (X Y Z : Type) (g : Y → Z) (f : X → Y) (x : X), g (f x)
-  , id := λ (X : Type) (x : X), x
-  , comp_assoc := take X Y Z W h g f, rfl
-  , comp_id := take X Y f, rfl
-  , id_comp := take X Y f, rfl
-  ⦄
+structure Functor (C D : Category) :=
+  (Ob : Category.Ob C → Category.Ob D)
+  (Hom : Π {a b : C}, Category.Hom C a b → Category.Hom D (Ob a) (Ob b))
+  (resp_comp : Π {a b c : C} {g : b ⇒ c} {f : a ⇒ b},
+               Hom (g ∘ f) = Hom g ∘ Hom f)
+  (resp_id : Π {a : C}, Hom (Category.id C a) = Category.id D (Ob a))
 
-structure functor (C D : Category) :=
-  (map_Ob : C → D)
-  (map_Hom : Π {a b : C}, Hom C a b → Hom D (map_Ob a) (map_Ob b))
-  (resp_comp : ∀ {a b c : C} {g : Hom C b c} {f : Hom C a b},
-               map_Hom (comp C g f) = comp D (map_Hom g) (map_Hom f))
-  (resp_id : ∀ {a : C}, map_Hom (@id C a) = @id D (map_Ob a))
+attribute Functor.Ob [coercion]
+attribute Functor.Hom [coercion]
 
-attribute functor.map_Ob [coercion]
-attribute functor.map_Hom [coercion]
+example (C D : Category) (F : Functor C D) (a b c : C) (g : b ⇒ c) (f : a ⇒ b) :
+  F (g ∘ f) = F g ∘ F f :=
+  Functor.resp_comp F
 
-structure nat_trans {C D : Category} (F G : functor C D) :=
-  (map : Π a : C, Hom D (F a) (G a))
-  (comm : ∀ {a b : C} {f : Hom C a b}, G f ∘ map a = map b ∘ F f)
+example (C D : Category) (F : Functor C D) (a : C) :
+  F (Category.id C a) = Category.id D (F a) :=
+  Functor.resp_id F
 
-attribute nat_trans.map [coercion]
+definition Functor.comp {C D E : Category} (G : Functor D E) (F : Functor C D) :
+  Functor C E :=
+  ⦃ Functor C E,
+    Ob := take a, G (F a),
+    Hom := take a b f, G (F f),
+    resp_comp := take a b c g f,
+      calc G (F (g ∘ f))
+           = G (F g ∘ F f)     : Functor.resp_comp F
+      ...  = G (F g) ∘ G (F f) : Functor.resp_comp G,
+    resp_id := take a,
+      calc G (F 1)
+           = G 1   : Functor.resp_id F
+      ...  = 1     : Functor.resp_id G ⦄
 
-definition functor_category (C D : Category) : Category :=
-  ⦃ Category
-  , Ob := functor C D
-  , Hom := λ F G : functor C D, nat_trans F G
-  , comp :=
-    λ F G H : functor C D,
-    λ eta : nat_trans G H,
-    λ xi : nat_trans F G,
-    ⦃ nat_trans F H 
-    , map := λ a : C, eta a ∘ xi a
-    , comm :=
-      take a b : C,
-      take f : Hom C a b,
+definition Functor.id (C : Category) : Functor C C :=
+  ⦃ Functor C C,
+    Ob := id,
+    Hom := λ a b, id,
+    resp_comp := take a b c g f, rfl,
+    resp_id := take a, rfl ⦄
+
+definition Cat : Category :=
+  ⦃ Category,
+    Ob := Category,
+    Hom := Functor,
+    comp := λ C D E, Functor.comp,
+    id := λ C, Functor.id C,
+    comp_assoc := sorry,
+    comp_id := sorry,
+    id_comp := sorry ⦄
+
+structure NatTrans {C D : Category} (F G : Functor C D) :=
+  (trans : Π a : C, F a ⇒ G a)
+  (comm : ∀ {a b : C} {f : a ⇒ b}, G f ∘ trans a = trans b ∘ F f)
+
+attribute NatTrans.trans [coercion]
+
+example (C D : Category) (F G : Functor C D) (xi : NatTrans F G) (a b : C) (f : a ⇒ b) :
+  G f ∘ xi a = xi b ∘ F f :=
+  NatTrans.comm xi
+
+definition NatTrans.comp {C D : Category} {F G H : Functor C D}
+  (eta : NatTrans G H) (xi : NatTrans F G) : NatTrans F H :=
+  ⦃ NatTrans F H,
+    trans := λ a, eta a ∘ xi a,
+    comm := take a b f,
       calc H f ∘ (eta a ∘ xi a)
-           = (H f ∘ eta a) ∘ xi a : comp_assoc
-      ...  = (eta b ∘ G f) ∘ xi a : nat_trans.comm eta
-      ...  = eta b ∘ (G f ∘ xi a) : comp_assoc
-      ...  = eta b ∘ (xi b ∘ F f) : nat_trans.comm xi
-      ...  = (eta b ∘ xi b) ∘ F f : comp_assoc
-    ⦄
-  , id :=
-    take F : functor C D,
-    ⦃ nat_trans F F
-    , map := λ a, id D
-    , comm :=
-      take a b : C,
-      take f : Hom C a b,
-      calc F f ∘ id D
-           = F f        : comp_id
-      ...  = id D ∘ F f : id_comp
-    ⦄
-  , comp_assoc := sorry
-  , comp_id := sorry
-  , id_comp := sorry
-  ⦄
+           = (H f ∘ eta a) ∘ xi a : Category.comp_assoc D
+      ...  = (eta b ∘ G f) ∘ xi a : NatTrans.comm eta
+      ...  = eta b ∘ (G f ∘ xi a) : Category.comp_assoc D
+      ...  = eta b ∘ (xi b ∘ F f) : NatTrans.comm xi
+      ...  = (eta b ∘ xi b) ∘ F f : Category.comp_assoc D ⦄
 
--- structure の等号を示す術を僕達はまだ知らない。
--- 関数の等号を外延性から示す術を僕達はまだ知らない。
+definition NatTrans.id {C D : Category} (F : Functor C D) : NatTrans F F :=
+  ⦃ NatTrans F F,
+    trans := λ a, 1,
+    comm := take a b f,
+      calc F f ∘ 1
+           = F f     : Category.comp_id D
+      ...  = 1 ∘ F f : Category.id_comp D ⦄
 
-definition presheaf (C : Category)
-  : Category
-  := functor_category (op C) Type_as_Category
+definition FuncCat (C D : Category) : Category :=
+  ⦃ Category,
+    Ob := Functor C D,
+    Hom := NatTrans,
+    comp := λ F G H, NatTrans.comp,
+    id := λ a, NatTrans.id a,
+    comp_assoc := sorry,
+    comp_id := sorry,
+    id_comp := sorry ⦄
 
-end Category  
+end category
