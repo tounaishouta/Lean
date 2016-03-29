96a016106c51559751a023ca0cb566e10ea73fdd
TOUNAI Shouta <tounai.shouta@gmail.com>
2016-03-29 23:45:52 +0900

    modify

--- category.lean
+++ category.lean
@@ -30,7 +30,7 @@ definition op (C : Category) : Category :=
     Ob := Category.Ob C,
     Hom := λ a b, Category.Hom C b a,
     comp := λ a b c g f, Category.comp C f g,
-    id := λ a, Category.id C a,
+    id := Category.id C,
     comp_assoc := take a b c d h g f, eq.symm (Category.comp_assoc C),
     comp_id := take a b f, Category.id_comp C,
     id_comp := take a b f, Category.comp_id C ⦄
@@ -39,8 +39,8 @@ definition Type_as_Cat : Category :=
   ⦃ Category,
     Ob := Type,
     Hom := λ a b, a → b,
-    comp := λ a b c g f, λ x, g (f x),
-    id := λ a, λ x, x,
+    comp := @function.comp,
+    id := @id,
     comp_assoc := take a b c d h g f, rfl,
     comp_id := take a b f, rfl,
     id_comp := take a b f, rfl ⦄
@@ -70,12 +70,12 @@ definition Functor.comp {C D E : Category} (G : Functor D E) (F : Functor C D) :
     Hom := take a b f, G (F f),
     resp_comp := take a b c g f,
       calc G (F (g ∘ f))
-           = G (F g ∘ F f)     : Functor.resp_comp F
-      ...  = G (F g) ∘ G (F f) : Functor.resp_comp G,
+           = G (F g ∘ F f)     : Functor.resp_comp F ...
+           = G (F g) ∘ G (F f) : Functor.resp_comp G,
     resp_id := take a,
       calc G (F 1)
-           = G 1   : Functor.resp_id F
-      ...  = 1     : Functor.resp_id G ⦄
+           = G 1   : Functor.resp_id F ... 
+           = 1     : Functor.resp_id G ⦄
 
 definition Functor.id (C : Category) : Functor C C :=
   ⦃ Functor C C,
@@ -84,15 +84,28 @@ definition Functor.id (C : Category) : Functor C C :=
     resp_comp := take a b c g f, rfl,
     resp_id := take a, rfl ⦄
 
+definition Functor.comp_assoc {C1 C2 C3 C4 : Category}
+  {F3 : Functor C3 C4} {F2 : Functor C2 C3} {F1 : Functor C1 C2} :
+  Functor.comp (Functor.comp F3 F2) F1 = Functor.comp F3 (Functor.comp F2 F1) :=
+  sorry
+
+definition Functor.comp_id {C D : Category} {F : Functor C D} :
+  Functor.comp F (Functor.id C) = F :=
+  sorry
+
+definition Functor.id_comp {C D : Category} {F : Functor C D} :
+  Functor.comp (Functor.id D) F = F :=
+  sorry
+
 definition Cat : Category :=
   ⦃ Category,
     Ob := Category,
     Hom := Functor,
-    comp := λ C D E, Functor.comp,
-    id := λ C, Functor.id C,
-    comp_assoc := sorry,
-    comp_id := sorry,
-    id_comp := sorry ⦄
+    comp := @Functor.comp,
+    id := Functor.id,
+    comp_assoc := @Functor.comp_assoc,
+    comp_id := @Functor.comp_id,
+    id_comp := @Functor.id_comp ⦄
 
 structure NatTrans {C D : Category} (F G : Functor C D) :=
   (trans : Π a : C, F a ⇒ G a)
@@ -100,38 +113,51 @@ structure NatTrans {C D : Category} (F G : Functor C D) :=
 
 attribute NatTrans.trans [coercion]
 
-example (C D : Category) (F G : Functor C D) (xi : NatTrans F G) (a b : C) (f : a ⇒ b) :
-  G f ∘ xi a = xi b ∘ F f :=
-  NatTrans.comm xi
+example (C D : Category) (F G : Functor C D) (ξ : NatTrans F G) (a b : C) (f : a ⇒ b) :
+  G f ∘ ξ a = ξ b ∘ F f :=
+  NatTrans.comm ξ
 
 definition NatTrans.comp {C D : Category} {F G H : Functor C D}
-  (eta : NatTrans G H) (xi : NatTrans F G) : NatTrans F H :=
+  (η : NatTrans G H) (ξ : NatTrans F G) : NatTrans F H :=
   ⦃ NatTrans F H,
-    trans := λ a, eta a ∘ xi a,
+    trans := λ a, η a ∘ ξ a,
     comm := take a b f,
-      calc H f ∘ (eta a ∘ xi a)
-           = (H f ∘ eta a) ∘ xi a : Category.comp_assoc D
-      ...  = (eta b ∘ G f) ∘ xi a : NatTrans.comm eta
-      ...  = eta b ∘ (G f ∘ xi a) : Category.comp_assoc D
-      ...  = eta b ∘ (xi b ∘ F f) : NatTrans.comm xi
-      ...  = (eta b ∘ xi b) ∘ F f : Category.comp_assoc D ⦄
+      calc H f ∘ (η a ∘ ξ a)
+           = (H f ∘ η a) ∘ ξ a : Category.comp_assoc D ...
+           = (η b ∘ G f) ∘ ξ a : NatTrans.comm η ...
+           = η b ∘ (G f ∘ ξ a) : Category.comp_assoc D ...
+           = η b ∘ (ξ b ∘ F f) : NatTrans.comm ξ ...
+           = (η b ∘ ξ b) ∘ F f : Category.comp_assoc D ⦄
 
 definition NatTrans.id {C D : Category} (F : Functor C D) : NatTrans F F :=
   ⦃ NatTrans F F,
     trans := λ a, 1,
     comm := take a b f,
       calc F f ∘ 1
-           = F f     : Category.comp_id D
-      ...  = 1 ∘ F f : Category.id_comp D ⦄
+           = F f     : Category.comp_id D ...
+           = 1 ∘ F f : Category.id_comp D ⦄
+
+definition NatTrans.comp_assoc {C D : Category} {F G H K : Functor C D}
+  {ζ : NatTrans H K} {η : NatTrans G H} {ξ : NatTrans F G} :
+  NatTrans.comp (NatTrans.comp ζ η) ξ = NatTrans.comp ζ (NatTrans.comp η ξ) :=
+  sorry
+
+definition NatTrans.comp_id {C D : Category} {F G : Functor C D} {ξ : NatTrans F G} :
+  NatTrans.comp ξ (NatTrans.id F) = ξ :=
+  sorry
+
+definition NatTrans.id_comp {C D : Category} {F G : Functor C D} {ξ : NatTrans F G} :
+  NatTrans.comp (NatTrans.id G) ξ = ξ :=
+  sorry
 
 definition FuncCat (C D : Category) : Category :=
   ⦃ Category,
     Ob := Functor C D,
     Hom := NatTrans,
-    comp := λ F G H, NatTrans.comp,
-    id := λ a, NatTrans.id a,
-    comp_assoc := sorry,
-    comp_id := sorry,
-    id_comp := sorry ⦄
+    comp := @NatTrans.comp C D,
+    id := NatTrans.id,
+    comp_assoc := @NatTrans.comp_assoc C D,
+    comp_id := @NatTrans.comp_id C D,
+    id_comp := @NatTrans.id_comp C D ⦄
 
 end category
